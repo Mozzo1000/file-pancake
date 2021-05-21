f6c194085a4fe66922df5a373897b6678e3b001b
Mozzo1000 <andreas@simplymozzo.se>
2021-05-21 17:18:27 +0200

    Add menu

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, qApp
+from PyQt5.QtWidgets import QMainWindow, QApplication, qApp, QMenu, QAction
 from PyQt5.QtCore import Qt
 from components.directorypane import DirectoryPane
 
@@ -12,8 +12,35 @@ class Gui(QMainWindow):
         directory_pane = DirectoryPane(self)
         self.addDockWidget(Qt.LeftDockWidgetArea, directory_pane)
 
+        self.create_menu()
+
         self.showMaximized()
         self.show()
+
+    def create_menu(self):
+        menu_bar = self.menuBar()
+        file_menu = QMenu('&File', self)
+        window_menu = QMenu('&Window', self)
+
+        exit_action = QAction('&Exit', self)
+        exit_action.triggered.connect(self.close)
+
+        new_explorer_window_action = QAction('&New explorer', self)
+        new_explorer_window_action.triggered.connect(self.new_explorer_window)
+
+
+        file_menu.addAction(exit_action)
+        window_menu.addAction(new_explorer_window_action)
+
+
+        menu_bar.addMenu(file_menu)
+        menu_bar.addMenu(window_menu)
+
+    def new_explorer_window(self):
+        directory_pane = DirectoryPane(self)
+        self.addDockWidget(Qt.RightDockWidgetArea, directory_pane)
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
