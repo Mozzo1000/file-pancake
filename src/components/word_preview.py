0a2bb1d7840651242d97a508f71f6d702ba5b043
Andreas Backström <andreas@simplymozzo.se>
2022-08-31 19:16:27 +0200

    Add property window to word preview

--- src/components/word_preview.py
+++ src/components/word_preview.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QLabel, QScrollArea
+from PyQt5.QtWidgets import QLabel, QScrollArea, QAction, QTreeWidget, QTreeWidgetItem, QDockWidget
+from PyQt5.QtCore import Qt
 from .preview_base import PreviewBase
 from docx2python import docx2python
 
@@ -7,7 +8,14 @@ class WordPreview(PreviewBase):
         super().__init__()
         self.allowed_file_types = ['doc', 'docx']
 
+        properties_button = QAction("Properties", self)
+        properties_button.triggered.connect(self.show_properties)
+        self._toolbar.addAction(properties_button)
+
         self.label = QLabel()
+        self.label.setTextFormat(Qt.RichText)
+        self.label.setWordWrap(True)
+
 
         scrollArea = QScrollArea()
         scrollArea.setWidget(self.label)
@@ -18,8 +26,19 @@ class WordPreview(PreviewBase):
     def set_file(self, file_full_path):
         super().set_file(file_full_path)
         f = docx2python(file_full_path, html=True)
-        print(f.body)
-
 
         self.label.setText(f.text)
         self.label.adjustSize()
+
+    def show_properties(self):
+        properties = docx2python(self.get_file_full_path()).core_properties
+        dock_widget = QDockWidget()
+        dock_widget.setWindowTitle("Properties -" +  self.get_file_full_path())
+        property_window = QTreeWidget()
+        property_window.setHeaderLabels(["Property", "Value"])
+        for key,value in properties.items():
+            property_item = QTreeWidgetItem([key, value])
+            property_window.addTopLevelItem(property_item)
+        dock_widget.setWidget(property_window)
+        self.base.addWidget(dock_widget)
+        
